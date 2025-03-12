8c7b8edbf306b48e64cd3f8b06cb7df05b9777f6
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-03-12 16:09:28 +0100

    add array with  five posts and res.json(posts)

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
 const express = require('express');
 const app = express();
-
 const PORT = 3000;
 
+// middleware
+app.use(express.static('public'));
+
+// define the route to home
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Server del mio blog')
 });
 
+//define api endpoint for 5 blog posts
+app.get('/bacheca/posts',(req, res) => {
+    const posts = [
+        {
+            title: "sdgasdg",
+            content: "asdgasg",
+            image: "asdgasdg",
+            tags: "gasdg"
+        },
+        {
+            title: "sdgasdg",
+            content: "asdgasg",
+            image: "asdgasdg",
+            tags: ["gasdg", "gds", "asf", "asfa", "asaf"]
+        },
+        {
+            title: "sdgasdg",
+            content: "asdgasg",
+            image: "asdgasdg",
+            tags: ["gasdg", "gds", "asf", "asfa", "asaf"]
+        },
+        {
+            title: "sdgasdg",
+            content: "asdgasg",
+            image: "asdgasdg",
+            tags: ["gasdg", "gds", "asf", "asfa", "asaf"]
+        },
+        {
+            title: "sdgasdg",
+            content: "asdgasg",
+            image: "asdgasdg",
+            tags: ["gasdg", "gds", "asf", "asfa", "asaf"]
+        }
+       
+    ];
+    res.json(posts)
+})
 
+//start server listen
 app.listen(PORT, () => {
     console.log(`Server run on port http://localhost:${PORT}`)
 })
